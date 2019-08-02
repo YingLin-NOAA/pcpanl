d73155d0c44f62bdaf7cdc4b51be88c2b5329bbb
Ying Lin <ying.lin@noaa.gov>
2019-08-02 19:19:20 +0000

    update

--- sorc/st4_oconus_grid_shift.fd/oconus_grid_shift.f
+++ sorc/st4_oconus_grid_shift.fd/oconus_grid_shift.f
@@ -4,7 +4,7 @@
 ! accumulated into 6h and 24 at NCEP - in case of PR QPE).  Assume that the 
 ! RFC's definition of the coordinate of the (1,1) box is the lower-left corner 
 ! of the box; redefine the grid so that the coordinate of the (1,1) box is 
-! the center of the box. 
+! the center of the box. Output it in GRIB2. 
 !
 ! Grid is assumed to be polar-stereographic (call to w3fb07)
 ! 
@@ -19,12 +19,16 @@
 !       we cannot be confident that some remote land areas in AK actually has
 !       coverage; having a land-only mask for AK might imply more confidence
 !       in the AKQPE than warranted).
+!   2019-07-12: output to GRIB2 rather than GRIB1.  
 !
       parameter(lmax=1000000)
       integer jpds(25), jgds(22), kpds(25), kgds(22)
       dimension pcp(lmax), bmask(lmax)
 !  bit is the dummy bitmap file (as a place holder in getgb, not actually used)
       logical*1  bit(lmax)
+      integer iptable(13)
+!
+      read(5,'(a10)',err=101) vdate
 !
 ! read in the input precip file using getgb:
 !
@@ -42,6 +46,13 @@
       alon1=float(kgds(5))/1000.
       alonv=kgds(7)/1000.           ! LOV grid orientation
       dx=kgds(8)
+!
+!
+!  Read in GRIB2 table to APCP:
+      do k = 1, 42
+        read(12,*)
+      end do
+      read (12,*) varnam, (iptable(i),i=1,13)
 !
 ! For PRQPE, create a bitmap mask by matching the value of the data mask to 
 !   the RFC ID in the GRIB header.  
@@ -64,6 +75,9 @@
       kpds(22)= 3       ! Decimal scale factor.  
       call baopenw(51,'fort.51',ibaret)
       call putgb(51,nx*ny,kpds,kgds,bit,pcp,iret)
+      call grib2_wrt_g2func(pcp,bit,vdate,1.,iptable,51,iret)
+      write(6,*) 'PUTGB to unit 51, iret=', iret,' iretba=', iretba
+      stop
       write(6,*) 'Write out shifted QPE file, ibaret,iret=',ibaret,iret
 !
       stop
