5b8179c0c6b2886c3ccb86cf9a58fff7a76a8437
YingLin-NOAA <ying.lin@noaa.gov>
2019-10-04 23:42:16 +0000

    update

--- util.dev/touch_ptmp_files.sh
+++ util.dev/touch_ptmp_files.sh
@@ -6,15 +6,19 @@ set -x
 #
 # each day go 'touch' the mrms and cmorph files on wexp grid for 
 #  $daym3,daym4,$daym5
+
+# 2019/10/04 temporarily adding daym6,$daym7 at Yan's request
+
 COMPCPANL=/ptmpp2/emc.rtmapara/com/pcpanl/para/pcpanl
 COMPCPURMA=/ptmpp2/emc.rtmapara/com/urma/para/pcpurma
 today=`date +%Y%m%d`
-
+daym7=`date +%Y%m%d -d "7 days ago"`
+daym6=`date +%Y%m%d -d "6 days ago"`
 daym5=`date +%Y%m%d -d "5 days ago"`
 daym4=`date +%Y%m%d -d "4 days ago"`
 daym4=`date +%Y%m%d -d "3 days ago"`
 
-for day in $daym5 $daym4 $daym3
+for day in $daym7 $daym6 $daym5 $daym4 $daym3
 do
   cd $COMPCPANL.$day
   touch hrlywgts*
